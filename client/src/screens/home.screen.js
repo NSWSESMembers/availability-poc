ec26e5687e5781f863ba89961a30c9edcb02d938
Andrew Short <andrewjshort@gmail.com>
2017-11-14 23:23:30 +1100

    Remove some unused code

--- client/src/screens/home.screen.js
+++ client/src/screens/home.screen.js
@@ -1,22 +1,10 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import {
-  FlatList,
-  ActivityIndicator,
-  Button,
-  Image,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-} from 'react-native';
-import { graphql, compose } from 'react-apollo';
-import moment from 'moment';
+import { StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { connect } from 'react-redux';
 
 import { appStyles } from './style-sheet';
-import CURRENT_USER_QUERY from '../graphql/current-user.query';
 
 const styles = StyleSheet.create(appStyles);
 
@@ -26,19 +14,7 @@ class Home extends Component {
     tabBarIcon: ({ tintColor}) => <Icon size={30} name={'home'} color={tintColor} />
   };
 
-  constructor(props) {
-    super(props);
-  }
-
-  onRefresh() {
-    this.props.refetch();
-  }
-
-  keyExtractor = item => item.id;
-
   render() {
-    const { loading, user, networkStatus } = this.props;
-
     return (
       <View style={styles.container}>
         <Text style={styles.warning}>{'Welcome home.'}</Text>
@@ -46,6 +22,7 @@ class Home extends Component {
     );
   }
 }
+
 Home.propTypes = {
   navigation: PropTypes.shape({
     navigate: PropTypes.func,
