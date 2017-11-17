3ab7d62aeb50d3032cae5925552f0c1d9e353123
OSPFNeighbour <ttdykes@gmail.com>
2017-11-17 15:28:13 +1100

    Hold onto your hats...
    Support promises for
    
    => signup mutation
    
    Added mutation for
    
    => deleteUser
    
    Added tests for:
    
    creating test user
    deleting test user

--- server/src/schema.js
+++ server/src/schema.js
@@ -47,6 +47,11 @@ export const Schema = [`
     email: String!
   }
 
+  input DeleteUserInput {
+    username: String!
+    email: String!
+  }
+
   type Organisation {
     id: Int! # unique id for the organisation
     name: String!
@@ -140,6 +145,7 @@ export const Schema = [`
   type Mutation {
     createGroup(group: CreateGroupInput!): Group
     createUser(user: CreateUserInput!): User
+    deleteUser(user: DeleteUserInput!): User
     createSchedule(schedule: CreateScheduleInput!): Schedule
     addUserToGroup(groupUpdate: AddUserToGroupInput!): Group
     createOrganisation(organisation: CreateOrganisationInput!): Organisation
