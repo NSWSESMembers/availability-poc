f215e3f920c01353b7682a88bb892b48dea86ad1
OSPFNeighbour <ttdykes@gmail.com>
2017-11-24 15:39:39 +1100

    Disable codepush in dev

--- client/index.ios.js
+++ client/index.ios.js
@@ -4,11 +4,13 @@ import {
 import codePush from 'react-native-code-push';
 import App from './src/app';
 
-const codePushOptions = {
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-  updateDialog: true,
-  installMode: codePush.InstallMode.ON_NEXT_RESUME,
-};
+if (!__DEV__) {
+  const codePushOptions = {
+    checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+    updateDialog: true,
+    installMode: codePush.InstallMode.ON_NEXT_RESUME,
+  };
+}
 
 AppRegistry.registerComponent(
   'availpoc',
