7e204cf0da0f94b75b0e759e3ec8ef97791e4274
Tim Dykes <ttdykes@gmail.com>
2017-11-25 19:28:09 +1100

    Disable codepush for dev builds (#59)
    Whenever `__DEV__` is `true` we set code push to do manual checks only which means that we won't be endlessly harassed about pending updates during development.

--- client/index.android.js
+++ client/index.android.js
@@ -5,7 +5,9 @@ import codePush from 'react-native-code-push';
 import App from './src/app';
 
 const codePushOptions = {
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  checkFrequency: (
+    (!__DEV__) ? codePush.CheckFrequency.ON_APP_RESUME
+      : codePush.CheckFrequency.MANUAL),
   updateDialog: true,
   installMode: codePush.InstallMode.ON_NEXT_RESUME,
 };
