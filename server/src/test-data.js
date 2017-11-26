d2edcaaaee3026881eeaa639e8ee629934c75445
Tim Dykes <ttdykes@gmail.com>
2017-11-26 11:28:59 +1100

    put a promise all under org in creation of test data (#56)

--- server/src/test-data.js
+++ server/src/test-data.js
@@ -4,93 +4,109 @@ export const loadTestData = () =>
   Creators.organisation({
     name: 'NSW SES',
   }).then(organisation =>
-    Creators.user({
-      id: 69,
-      username: 'chris',
-      password: 'testing',
-      email: 'test@miceli.net.au',
-      organisation,
-    }).then(user =>
-      Promise.all([
-        Creators.device({
-          uuid: '1234abc',
-          user,
-        }),
-        Creators.capability({
-          name: 'Road Crash Rescue',
-          organisation,
-          user,
-        }),
-        Creators.capability({
-          name: 'Vertical Rescue',
-          organisation,
-          user,
-        }),
-        Creators.capability({
-          name: 'Flood Rescue',
-          organisation,
-          user,
-        }),
-        Creators.capability({
-          name: 'Chainsaw',
-          organisation,
-          user,
-        }),
-        Creators.group({
-          name: 'Kiama',
-          user,
-          organisation,
-        }),
-        Creators.group({
-          name: 'Parramatta',
-          user,
-          organisation,
-        }),
-        Creators.group({
-          name: 'Wollongong',
-          organisation,
-          user,
-        }).then(group => Promise.all([
-          Creators.schedule({
-            group,
-            name: 'Wollongong S&W',
-            details: 'Weekly storm availability',
+    Promise.all([
+      Creators.user({
+        id: 68,
+        username: 'tim',
+        password: '123',
+        email: 'test@test.com',
+        organisation,
+      }),
+      Creators.user({
+        id: 69,
+        username: 'chris',
+        password: 'testing',
+        email: 'test@miceli.net.au',
+        organisation,
+      }).then(user =>
+        Promise.all([
+          Creators.user({
+            id: 70,
+            username: 'tim',
+            password: '123',
+            email: 'tim@tim.com',
+            organisation,
           }),
-          Creators.schedule({
-            group,
-            name: 'Wollongong VR',
-            details: 'Weekly VR availability',
+          Creators.device({
+            uuid: '1234abc',
+            user,
           }),
-          Creators.schedule({
-            group,
-            name: 'Emergency Services Expo',
-            details: '5 people needed for demonstrations on Sunday',
-          }),
-          Creators.tag({
-            name: 'Wollongong City',
+          Creators.capability({
+            name: 'Road Crash Rescue',
             organisation,
             user,
-            group,
           }),
-          Creators.tag({
-            name: 'Corrimal',
+          Creators.capability({
+            name: 'Vertical Rescue',
             organisation,
             user,
-            group,
           }),
-          Creators.event({
-            name: 'VR Wollongong Escarpment',
-            details: 'VR operators required to assist with rescue',
-            group,
+          Creators.capability({
+            name: 'Flood Rescue',
+            organisation,
+            user,
           }),
-          Creators.event({
-            name: 'Assist ASNSW',
-            details: 'Needed to carry patient over rough terrain',
-            group,
+          Creators.capability({
+            name: 'Chainsaw',
+            organisation,
+            user,
           }),
-        ])),
-      ]),
-    ),
+          Creators.group({
+            name: 'Kiama',
+            user,
+            organisation,
+          }),
+          Creators.group({
+            name: 'Parramatta',
+            user,
+            organisation,
+          }),
+          Creators.group({
+            name: 'Wollongong',
+            organisation,
+            user,
+          }).then(group => Promise.all([
+            Creators.schedule({
+              group,
+              name: 'Wollongong S&W',
+              details: 'Weekly storm availability',
+            }),
+            Creators.schedule({
+              group,
+              name: 'Wollongong VR',
+              details: 'Weekly VR availability',
+            }),
+            Creators.schedule({
+              group,
+              name: 'Emergency Services Expo',
+              details: '5 people needed for demonstrations on Sunday',
+            }),
+            Creators.tag({
+              name: 'Wollongong City',
+              organisation,
+              user,
+              group,
+            }),
+            Creators.tag({
+              name: 'Corrimal',
+              organisation,
+              user,
+              group,
+            }),
+            Creators.event({
+              name: 'VR Wollongong Escarpment',
+              details: 'VR operators required to assist with rescue',
+              group,
+            }),
+            Creators.event({
+              name: 'Assist ASNSW',
+              details: 'Needed to carry patient over rough terrain',
+              group,
+            }),
+          ])),
+        ]),
+      ),
+    ]),
   );
 
 export default loadTestData;
