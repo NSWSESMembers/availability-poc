12b079f1877b764bdce2ac93c3acbd290b500ddc
Sam Dunster <me@sdunster.com>
2017-11-26 16:14:49 -0800

    Default to using local GraphQL server if __DEV__
    * When `__DEV__` is true (development builds of the app/react native bundler is running) we default to using `http://localhost:8080/graphql`.
    * This might not work for everyone but that is OK - they can change it.
    * If someone accidentally commits their local IP or whatever it won't affect production builds.
    * Closes #63

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,5 @@
-export const GRAPHQL_ENDPOINT = 'https://ses-availability-api.herokuapp.com/graphql';
+export const GRAPHQL_ENDPOINT_PROD = 'https://ses-availability-api.herokuapp.com/graphql';
+export const GRAPHQL_ENDPOINT_LOCAL = 'http://localhost:8080/graphql';
+
+export const GRAPHQL_ENDPOINT = __DEV__ ? GRAPHQL_ENDPOINT_LOCAL : GRAPHQL_ENDPOINT_PROD;
 export default GRAPHQL_ENDPOINT;
