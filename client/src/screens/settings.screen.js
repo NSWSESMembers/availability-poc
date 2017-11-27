7bd172c070aef17fc28964abee60106e7543ff51
Sam Dunster <me@sdunster.com>
2017-11-26 16:41:10 -0800

    Client package upgrades/modernisation
    * Held back the following:
      * apollo-client - still beta
      * react-apollo - depends on new version of apollo-client
      * react - react-native wants 16.0.0 specifically
    * Adopt prop-types because React v16 will require it
    * Adopt new redux-persist API
    * Update project files to match react-native changes.
      * Did not adopt unified index.js - we'll do this later so as not to break code push.
    * More error checking in local state reducers.
    * Remove `redux-devtools-extension` since it is designed for browser use.
      * We could add `remote-redux-devtools` and `remotedev-rn-debugger` later.

--- client/src/screens/settings.screen.js
+++ client/src/screens/settings.screen.js
@@ -1,5 +1,6 @@
 /* global navigator */
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import {
   ActivityIndicator,
   Alert,
