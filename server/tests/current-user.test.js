302d322a937df2e94862bdaa86af1a0d778b31f1
OSPFNeighbour <ttdykes@gmail.com>
2017-11-30 10:16:03 +1100

    lists to test query with filters passed on org and user

--- server/tests/current-user.test.js
+++ server/tests/current-user.test.js
@@ -67,6 +67,29 @@ describe('GraphQL query - Current user', () => {
     }));
   });
 
+  describe('Get organisations groups passing filter', () => {
+    const response = query(`
+      {
+        user {
+          organisation {
+            id
+            name
+            groups(id: 1) {
+              id
+            }
+          }
+        }
+      }
+    `);
+
+    itReturnsSuccess(response);
+    it('Returns correct single group entity', () => response.then((res) => {
+      expect(res.data.user.organisation.groups.length).toBe(1);
+      expect(res.data.user.organisation.groups[0]).toHaveProperty('id');
+      expect(res.data.user.organisation.groups[0].id).toBe(1);
+    }));
+  });
+
   describe('Get groups', () => {
     const response = query(`
       {
@@ -89,6 +112,35 @@ describe('GraphQL query - Current user', () => {
     `);
 
     itReturnsSuccess(response);
+  });
+
+  describe('Get users group by passing filter', () => {
+    const response = query(`
+      {
+        user {
+          id
+          groups(id: 1) {
+            id
+            schedules {
+              id
+            }
+            events {
+              id
+            }
+            tags {
+              id
+            }
+          }
+        }
+      }
+    `);
+
+    itReturnsSuccess(response);
+    it('Returns correct single entity', () => response.then((res) => {
+      expect(res.data.user.groups.length).toBe(1);
+      expect(res.data.user.groups[0]).toHaveProperty('id');
+      expect(res.data.user.groups[0].id).toBe(1);
+    }));
   });
 
   describe('Get schedules', () => {
