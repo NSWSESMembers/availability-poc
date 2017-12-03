851a02dc32b26c5970ae4742e7f0ef2da516c11a
Sam Dunster <me@sdunster.com>
2017-12-02 19:05:58 -0800

    Rebuild app, rename Callout
    * Deleted ios and android directories
    * Re-built from scratch with `react-native eject`
    * Merged `index.ios.js` and `index.android.js` into `index.js` as per latest React Native recommendations.
    * Re-linked plugins and configured:
      * `react-native-maps`
      * `react-native-vector-icons`
      * Code Push
      * Geolocation
    * Added some really shitty placeholder icons
    * App now named 'Callout' for now

--- client/ios/CalloutTests/CalloutTests.m
+++ client/ios/CalloutTests/CalloutTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface availpocTests : XCTestCase
+@interface CalloutTests : XCTestCase
 
 @end
 
-@implementation availpocTests
+@implementation CalloutTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
