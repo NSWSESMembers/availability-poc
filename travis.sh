07ebb252eaf4899e1e47d42010d576f1a799a4e7
Sam Dunster <me@sdunster.com>
2017-12-03 15:09:28 -0800

    Fix travis races
    * `sleep 10` after launching the server - giving it time to boot and load test data.

--- travis.sh
+++ travis.sh
@@ -13,6 +13,10 @@ then
 
   echo "Starting server..."
   yarn run serve &
+
+  # wait 10 seconds for the server to boot and load the test data
+  # TODO: run the server in-process
+  sleep 10
 fi
 
 echo "Running $MODE on $COMPONENT..."
