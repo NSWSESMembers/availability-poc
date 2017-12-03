7b1fc22ffe43cff78519406f2cc1f7625d83f675
Sam Dunster <me@sdunster.com>
2017-12-03 14:54:09 -0800

    API for getting events/schedules by ID
    * Respects organisation permissions (you must be a member of the event's group's organisation)
      * New `perms.js` to store this logic
      * Complete unit test coverage
    * New unit tests require beta version of `jest` due to bug checking for rejected Promises.

---
+++ server/src/perms.test.js
@@ -0,0 +1,149 @@
+import { schedulePerms, eventPerms } from './perms';
+
+describe('Schedule permissions', () => {
+  describe('allowed', () => {
+    const user = {
+      organisationId: 69,
+    };
+    const schedule = {
+      getGroup: () => ({
+        organisationId: 69,
+      }),
+    };
+
+    it('with no promises', () =>
+      expect(schedulePerms.userWantsToRead({ user, schedule }))
+        .resolves.toBe(schedule),
+    );
+
+    it('with promises', () =>
+      expect(schedulePerms.userWantsToRead({
+        user: Promise.resolve(user),
+        schedule: Promise.resolve(schedule),
+      }))
+        .resolves.toBe(schedule),
+    );
+  });
+
+  describe('denied', () => {
+    it('wrong org', () => {
+      const user = {
+        organisationId: 69,
+      };
+      const schedule = {
+        getGroup: () => ({
+          organisationId: 1,
+        }),
+      };
+
+      return expect(schedulePerms.userWantsToRead({ user, schedule }))
+        .rejects.toThrow();
+    });
+
+    it('invalid group', () => {
+      const user = {
+        organisationId: 69,
+      };
+      const schedule = {
+        getGroup: () => null,
+      };
+
+      return expect(schedulePerms.userWantsToRead({ user, schedule }))
+        .rejects.toThrow();
+    });
+
+    it('invalid user', () => {
+      const schedule = {
+        getGroup: () => ({
+          organisationId: 1,
+        }),
+      };
+
+      return expect(schedulePerms.userWantsToRead({ user: null, schedule }))
+        .rejects.toThrow();
+    });
+
+    it('invalid schedule', () => {
+      const user = {
+        organisationId: 69,
+      };
+
+      return expect(schedulePerms.userWantsToRead({ user, schedule: null }))
+        .rejects.toThrow();
+    });
+  });
+});
+
+describe('Event permissions', () => {
+  describe('allowed', () => {
+    const user = {
+      organisationId: 69,
+    };
+    const event = {
+      getGroup: () => ({
+        organisationId: 69,
+      }),
+    };
+
+    it('with no promises', () =>
+      expect(eventPerms.userWantsToRead({ user, event }))
+        .resolves.toBe(event),
+    );
+
+    it('with promises', () =>
+      expect(eventPerms.userWantsToRead({
+        user: Promise.resolve(user),
+        event: Promise.resolve(event),
+      }))
+        .resolves.toBe(event),
+    );
+  });
+
+  describe('denied', () => {
+    it('wrong org', () => {
+      const user = {
+        organisationId: 69,
+      };
+      const event = {
+        getGroup: () => ({
+          organisationId: 1,
+        }),
+      };
+
+      return expect(eventPerms.userWantsToRead({ user, event }))
+        .rejects.toThrow();
+    });
+
+    it('invalid group', () => {
+      const user = {
+        organisationId: 69,
+      };
+      const event = {
+        getGroup: () => null,
+      };
+
+      return expect(eventPerms.userWantsToRead({ user, event }))
+        .rejects.toThrow();
+    });
+
+    it('invalid user', () => {
+      const event = {
+        getGroup: () => ({
+          organisationId: 1,
+        }),
+      };
+
+      return expect(eventPerms.userWantsToRead({ user: null, event }))
+        .rejects.toThrow();
+    });
+
+    it('invalid event', () => {
+      const user = {
+        organisationId: 69,
+      };
+
+      return expect(eventPerms.userWantsToRead({ user, event: null }))
+        .rejects.toThrow();
+    });
+  });
+});
