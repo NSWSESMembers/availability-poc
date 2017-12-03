0001ccbf9be9da7064a387a561606628efcb9fa2
OSPFNeighbour <ttdykes@gmail.com>
2017-12-03 22:31:58 +1100

    refactor #88 tests into new groups file

--- server/tests/groups.test.js
+++ server/tests/groups.test.js
@@ -1,4 +1,34 @@
-import { itReturnsSuccess, run } from './common';
+import { itReturnsSuccess, run, query } from './common';
+
+describe('Get users group by passing filter', () => {
+  const response = query(`
+    {
+      user {
+        id
+        groups(id: 2) {
+          id
+          schedules {
+            id
+          }
+          events {
+            id
+          }
+          tags {
+            id
+          }
+        }
+      }
+    }
+  `);
+
+  itReturnsSuccess(response);
+  it('Returns correct single entity', () => response.then((res) => {
+    expect(res.data.user.groups.length).toBe(1);
+    expect(res.data.user.groups[0]).toHaveProperty('id');
+    expect(res.data.user.groups[0].id).toBe(2);
+    expect(res.data.user.groups[0].id).not.toBe(1);
+  }));
+});
 
 describe('Manipulate user group membership - leave', () => {
   const response = run(
@@ -52,7 +82,6 @@ describe('Manipulate user group membership - join', () => {
   itReturnsSuccess(response);
   it('should contain joined group', () => response.then((res) => {
     expect(res.data.addUserToGroup).toHaveProperty('name');
-    expect(res.data.addUserToGroup.name).toBe('Kiama');
     expect(res.data.addUserToGroup.users).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
