e3fa19f834baa3015f82ecec1731e0b764a38359
Sam Dunster <me@sdunster.com>
2018-01-07 20:51:07 -0800

    Deploy master branch automatically to heroku
    * Use travis build stages to ensure the tests pass before we push.
    * New build.sh script to transpile and bundle.
    * We ship only the transpiled source, `package.json`, `yarn.lock` and `Procfile` to heroku which should allow for faster dyno start times.
    * No longer doing weird git subtree stuff - just upload tarball direct from travis.

---
+++ server/scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is made to build a self-contained dist/ directory that contains
+# a package.json ready for the nodejs buildpack on heroku but you could also
+# run it elswhere by running: yarn && node main.js
+
+echo "Transpiling with babel..."
+babel src -d dist --ignore '__tests__'
+
+for i in package.json yarn.lock Procfile
+do
+  echo "Copying metadata ($i)..."
+  cp "$i" "dist/$i"
+done
