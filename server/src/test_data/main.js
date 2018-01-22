180c0ee6065853b6a89f898e5d2d1cf6ddda4994
OSPFNeighbour <ttdykes@gmail.com>
2018-01-22 14:33:52 +1100

    cleanup test cases

--- server/src/test_data/main.js
+++ server/src/test_data/main.js
@@ -29,6 +29,12 @@ export const loadTestData = (Creators, models) =>
           organisation,
         }).then((group) => {
           Promise.all([
+            Creators.tag({
+              name: 'Wollongong City',
+              organisation,
+              user,
+              group,
+            }),
             Creators.event({
               name: `Rescue of ${faker.name.jobType()}`,
               details: faker.hacker.phrase(),
@@ -83,7 +89,81 @@ export const loadTestData = (Creators, models) =>
             name: 'State Group',
             user,
             organisation,
-          }),
+          }).then(group => Promise.all([
+            Creators.schedule({
+              group,
+              name: 'Wollongong S&W',
+              details: 'Weekly storm availability',
+              startTime: 0, // ongoing
+              endTime: distantFuture, // ongoing
+            }).then(schedule =>
+              Creators.timeSegment({
+                status: 'Available',
+                startTime: nowInUTC() + 3600, // +1 hr
+                endTime: nowInUTC() + (3600 * 2), // +2 hr
+                schedule,
+                user,
+              }),
+            ),
+            Creators.schedule({
+              group,
+              name: 'Wollongong VR',
+              details: 'Weekly VR availability',
+              startTime: 0, // ongoing
+              endTime: distantFuture, // ongoing
+            }),
+            Creators.schedule({
+              group,
+              name: 'Emergency Services Expo',
+              details: '5 people needed for demonstrations on Sunday',
+              startTime: 1512860400, // Sunday 10/12/17 @ 10am AEST in GMT
+              endTime: 1512878400, // Sunday 10/12/17 @ 3pm AEST in GMT
+            }),
+            Creators.tag({
+              name: 'Wollongong City',
+              organisation,
+              user,
+              group,
+            }),
+            Creators.tag({
+              name: 'Corrimal',
+              organisation,
+              user,
+              group,
+            }),
+            Creators.event({
+              name: 'VR Wollongong Escarpment',
+              details: 'VR operators required to assist with rescue',
+              group,
+            }).then(event =>
+              Promise.all([
+                Creators.eventResponse({
+                  status: 'Responding',
+                  detail: 'ETA 10 min',
+                  destination: 'Scene',
+                  eta: 1517443200,
+                  event,
+                  user,
+                }),
+                Creators.eventResponse({
+                  status: 'Responding',
+                  detail: 'WOL39',
+                  destination: 'Scene',
+                  eta: 1517443200,
+                  event,
+                  user,
+                }),
+                Creators.eventResponse({
+                  status: 'Unavailable',
+                  detail: 'At work',
+                  destination: '',
+                  eta: 1517443200,
+                  event,
+                  user,
+                }),
+              ]),
+            ),
+          ])),
           Creators.device({
             uuid: '1234abc',
             user,
