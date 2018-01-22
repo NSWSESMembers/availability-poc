22c51ff25d31f9a9eeff5f39b786db2df58bf5d1
OSPFNeighbour <ttdykes@gmail.com>
2018-01-23 10:52:55 +1100

    constants testing

--- server/src/__tests__/constants.js
+++ server/src/__tests__/constants.js
@@ -4,3 +4,11 @@ it('exists', () => {
   expect(distantFuture).toBeDefined();
   expect(nowInUTC).toBeDefined();
 });
+
+it('nowInUTC returns a number', () =>
+  expect(nowInUTC()).toBeGreaterThan(0),
+);
+
+it('distantFuture returns a number', () =>
+  expect(distantFuture).toEqual(2147483647),
+);
