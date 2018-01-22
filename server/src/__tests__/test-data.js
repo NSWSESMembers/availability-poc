b07f80596c2275b520cbee5179428afac1dfcf56
OSPFNeighbour <ttdykes@gmail.com>
2018-01-22 13:08:59 +1100

    tests fix

--- server/src/__tests__/test-data.js
+++ server/src/__tests__/test-data.js
@@ -1,10 +1,10 @@
-import { loadTestData } from '../test-data';
-import { defineModels } from '../models-mock';
-import { getCreators } from '../creators';
+import { loadTestData } from '../test_data/main';
+import { defineModels } from './models';
+import { getCreators } from './creators';
 
 const models = defineModels();
 const creators = getCreators(models);
 
 it('succeeds', async () => {
-  await loadTestData(creators);
+  await loadTestData(creators, models);
 });
