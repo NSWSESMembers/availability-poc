fc894129923296d9d06c54eeae2c1e1a9b182046
Tim Dykes <ttdykes@gmail.com>
2018-01-31 23:23:58 +1100

    fixed broken leave group code so that you can leave a group. (#163)
    fixed broken leave group code so that you can leave a group.

--- client/src/graphql/leave-group.mutation.js
+++ client/src/graphql/leave-group.mutation.js
@@ -2,6 +2,29 @@ import gql from 'graphql-tag';
 
 export default gql`
   mutation removeUserFromGroup($groupUpdate: RemoveUserFromGroupInput!) {
-    removeUserFromGroup(groupUpdate: $groupUpdate)
+    removeUserFromGroup(groupUpdate: $groupUpdate) {
+      id
+      name
+      tags {
+         name
+         id
+       }
+       users {
+         username
+         id
+       }
+       events {
+         id
+         name
+         details
+       }
+       schedules {
+         id
+         name
+         details
+         startTime
+         endTime
+       }
+    }
   }
 `;
--- server/src/logic.js
+++ server/src/logic.js
@@ -382,7 +382,7 @@ export const getHandlers = ({ models, creators: Creators }) => {
               return Promise.reject(Error('Invalid group!'));
             }
             return group.removeUser(user).then((rows) => {
-              if (rows) { return true; }
+              if (rows) { return group; }
               return false;
             });
           }),
