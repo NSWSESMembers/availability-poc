ab8f58823419eec4a1bd9a66d3d06c082f0c1c6d
Sam Dunster <me@sdunster.com>
2018-02-09 21:57:58 -0700

    Edit event responses and sync to server
    * Change respond status (Available/Unavailable/Tentative)
    * Refetch event when there is a successful update.
    * Submit location at most every 4 seconds when you are responding to the event that is currently on-screen.

---
+++ client/src/graphql/event-response.fragment.js
@@ -0,0 +1,19 @@
+import gql from 'graphql-tag';
+
+const EVENT_RESPONSE_FRAGMENT = gql`
+  fragment EventResponseFragment on EventResponse {
+    status
+    detail
+    destination
+    eta
+    locationLatitude
+    locationLongitude
+    user {
+      id
+      username
+      displayName
+    }
+  }
+`;
+
+export default EVENT_RESPONSE_FRAGMENT;
--- client/src/graphql/set-event-response.mutation.js
+++ client/src/graphql/set-event-response.mutation.js
@@ -1,12 +1,12 @@
 import gql from 'graphql-tag';
 
-import EVENT_FRAGMENT from './event.fragment';
+import EVENT_RESPONSE_FRAGMENT from './event-response.fragment';
 
 export default gql`
   mutation setEventResponse($response: SetEventResponseInput!) {
     setEventResponse(response: $response) {
-      ... EventFragment
+      ... EventResponseFragment
     }
   }
-  ${EVENT_FRAGMENT}
+  ${EVENT_RESPONSE_FRAGMENT}
 `;
