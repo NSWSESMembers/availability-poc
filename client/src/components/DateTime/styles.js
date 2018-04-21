965a61c324eaad2be19c8bf9f8ac79e28ee0f07f
OSPFNeighbour <ttdykes@gmail.com>
2018-04-21 19:54:58 +1000

    strike through the text of days that are out of range

--- client/src/components/DateTime/styles.js
+++ client/src/components/DateTime/styles.js
@@ -46,6 +46,7 @@ const styles = StyleSheet.create({
   },
   dayLabel: {},
   dayLabelOutOfRange: {
+    textDecorationLine: 'line-through',
     color: Colors.bgOutOfRange,
   },
   dayLabelAvailable: {
