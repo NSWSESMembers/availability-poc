1c4ca8a9399e637abea5314f5cf22cba7d034f89
OSPFNeighbour <ttdykes@gmail.com>
2018-04-22 19:47:09 +1000

    The internet says this is the easy way to do it, and will give a minor increase in performance

--- client/src/app.js
+++ client/src/app.js
@@ -15,6 +15,14 @@ import local from './state/local.reducer';
 import { logout } from './state/auth.actions';
 import { GRAPHQL_ENDPOINT } from './config';
 
+function noop() {}
+
+if (process.env.NODE_ENV !== 'development') {
+  console.log = noop;
+  console.warn = noop;
+  console.error = noop;
+}
+
 console.log(`Using GraphQL endpoint: ${GRAPHQL_ENDPOINT}`);
 const networkInterface = createNetworkInterface({ uri: GRAPHQL_ENDPOINT });
 let store;
