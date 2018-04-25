19a6c5f1574d3a1253d92383baf955df87dae9f7
Sam Dunster <me@sdunster.com>
2018-04-25 15:19:08 -0700

    Remove unused components
    * Remove `<ListItemAction>`
    * Remove `<TextInputRow>`

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -1,7 +1,5 @@
 import ListItem from './ListItem';
 import ListItemHighlight from './ListItemHighlight';
-
-import ListItemAction from './ListItemAction';
 import styles from './styles';
 
-export { ListItem, ListItemHighlight, ListItemAction, styles };
+export { ListItem, ListItemHighlight, styles };
