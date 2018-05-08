617ece33990844466dee416c3e6eb5f8b9f13744
Tim Dykes <ttdykes@gmail.com>
2018-05-08 15:18:37 +0530

    toast for copied to clipboard action on internal params screen (#367)
    toast for copied to clipboard action on internal params screen

--- client/src/screens/burger/components/ParamsListItem.js
+++ client/src/screens/burger/components/ParamsListItem.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Clipboard } from 'react-native';
+import { Clipboard, Platform, ToastAndroid } from 'react-native';
 
 import { ListItem } from '../../../components/List';
 
@@ -8,6 +8,9 @@ import { ListItem } from '../../../components/List';
 const ParamsListItem = ({ title, detail }) => {
   const onPress = () => {
     Clipboard.setString(detail);
+    if (Platform.OS === 'android') {
+      ToastAndroid.show('Copied to clipboard', ToastAndroid.SHORT);
+    }
   };
 
   return (
