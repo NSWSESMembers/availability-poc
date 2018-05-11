de7393cc50ce6e3d4c6d103b2b393334feb16661
OSPFNeighbour <ttdykes@gmail.com>
2018-05-11 11:22:34 +0530

    Fixed GPS location not updating. Added Halo circle to show location accuracy

--- client/src/components/MapMarker/index.js
+++ client/src/components/MapMarker/index.js
@@ -1,7 +1,8 @@
 import UserMarker from './UserMarker';
 import MyLocationMarker from './MyLocationMarker';
 import IconMarker from './IconMarker';
+import AccuracyHalo from './AccuracyHalo';
 
 import styles from './styles';
 
-export { UserMarker, IconMarker, MyLocationMarker, styles };
+export { UserMarker, IconMarker, MyLocationMarker, AccuracyHalo, styles };
