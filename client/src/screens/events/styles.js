cb0a2fafa8863b79957435a25ccab3ce5d5be980
OSPFNeighbour <ttdykes@gmail.com>
2018-05-14 09:47:01 +1000

    moved stye to seperate file

---
+++ client/src/screens/events/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+  map: {
+    backgroundColor: '#f9f9f9',
+    flexGrow: 1,
+    zIndex: -1,
+    ...StyleSheet.absoluteFillObject,
+  },
+
+});
+
+
+export default styles;
